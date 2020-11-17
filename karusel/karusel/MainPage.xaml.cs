7ce0e5a76a6babde04774796a09814a95851bea9
Opilane TTHK <opilane@tthk.ee>
2020-11-17 12:21:38 +0200

    Add project files.

---
+++ karusel/karusel/MainPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace karusel
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var redContentPage = new ContentPage
+
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "red",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+
+                        },
+
+                        new BoxView
+                        {
+                            Color = Color.Red,
+                            WidthRequest = 200,
+                            HeightRequest = 200,
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.CenterAndExpand
+                        }
+
+
+                    }
+                }
+
+            };
+            //------------------------------
+            var blackContentPage = new ContentPage
+            {
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Black",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+
+                        },
+
+                        new BoxView
+                        {
+                            Color = Color.Black,
+                            WidthRequest = 200,
+                            HeightRequest = 200,
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.CenterAndExpand
+                        }
+
+                    }
+                    }
+                
+
+                };
+
+
+            //-----------------
+            var blackContentPage = new ContentPage
+            {
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Black",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+
+                        },
+
+                        new BoxView
+                        {
+                            Color = Color.Black,
+                            WidthRequest = 200,
+                            HeightRequest = 200,
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.CenterAndExpand
+                        }
+
+                    }
+                    }
+                    };
+                    Children.Add(redContentPage);
+                    Children.Add(blackContentPage);
+            Children.Add(purpleContentPage);
+                }
+    }
+}
+    }
+
+
+
+
+
+
+
